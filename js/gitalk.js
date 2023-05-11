8dd27f08518baa3a898a884f828588628d291b05
211xxa <3114921075@qq.com>
2023-05-11 15:11:25 +0800

    Site updated: 2023-05-11 15:11:24

--- js/gitalk.js
+++ js/gitalk.js
@@ -1,9 +1,9 @@
 const gitalk = new Gitalk({
-    clientID: '1edcf51120d78d14fafb',
-    clientSecret: '2709738244ad1c73a3a9b17383995bc4fe983c89',
-    repo: 'pinglun.github.io',      // The repository of store comments,
-    owner: '211xxa',
-    admin: ['pinglun.github.io'],
+    clientID: 'GitHub Application Client ID',
+    clientSecret: 'GitHub Application Client Secret',
+    repo: 'GitHub repo',      // The repository of store comments,
+    owner: 'GitHub repo owner',
+    admin: ['GitHub repo owner and collaborators, only these guys can initialize github issues'],
     id: location.pathname,      // Ensure uniqueness and length less than 50
     distractionFreeMode: false  // Facebook-like distraction free mode
 })
